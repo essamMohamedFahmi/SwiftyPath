e3fe05011ef74dd2b7d4f1647435b72e90ab14e1
Essam Mohamed Fahmi <essammohamedomran@gmail.com>
2020-10-01 22:38:31 +0200

    Initial Commit

---
+++ SwiftyPath/SwiftyPath.h
@@ -0,0 +1,18 @@
+//
+//  SwiftyPath.h
+//  SwiftyPath
+//
+//  Created by Essam Mohamed Fahmi on 10/1/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftyPath.
+FOUNDATION_EXPORT double SwiftyPathVersionNumber;
+
+//! Project version string for SwiftyPath.
+FOUNDATION_EXPORT const unsigned char SwiftyPathVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyPath/PublicHeader.h>
+
+
